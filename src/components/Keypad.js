7295f73073bffeb713c9abdb11f802a889f759b9
Arthur V Fain <arthurvfain@gmail.com>
2021-06-13 20:03:45 -0400

    commit tree 020bb2cb883e5efbad857945d97974f709c1ba29

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+function Keypad()
+{
+    function passwordHandler(e)
+    {
+        console.log(`Entering password...`)
+    }
+    return (
+        <form>
+            <input onChange={passwordHandler} type="password"/>
+        </form>
+    )
+}
+
+export default Keypad
